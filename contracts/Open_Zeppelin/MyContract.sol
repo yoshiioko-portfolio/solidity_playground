494d44bf24fe4579a243fa75db1b8ad018fa6a6e
yoshiioko <acardoza@pdx.edu>
2022-04-28 23:37:30 -0700

    Add: file required for MyContract

---
+++ contracts/Open_Zeppelin/MyContract.sol
@@ -0,0 +1,22 @@
+// Example of how to use Ownable
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyContract is Ownable {
+    uint count = 0;
+
+    function getCount() public view returns (uint) {
+        // Anyone can call this function
+        return count;
+    }
+
+    function increment() public onlyOwner {
+        count = count + 1;
+    }
+
+    function decrement() public onlyOwner {
+        count = count - 1;
+    }
+}
