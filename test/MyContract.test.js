96373b132d426caf78e6fc3680c9a3041cc433bb
yoshiioko <acardoza@pdx.edu>
2022-04-28 23:43:04 -0700

    Update: Implement decrement for non-owner test.

--- test/MyContract.test.js
+++ test/MyContract.test.js
@@ -14,13 +14,11 @@ contract("MyContract", function (accounts) {
     this.contract = await MyContract.new({ from: owner });
   });
 
-  // Test case 1
   it("Test initial count", async function () {
     let init_count = await this.contract.getCount();
     expect(init_count.toString()).to.equal("0");
   });
 
-  // Test case 2
   it("Owner increment the count twice", async function () {
     await this.contract.increment();
     await this.contract.increment();
@@ -29,7 +27,6 @@ contract("MyContract", function (accounts) {
     expect(curr_count.toString()).to.equal("2");
   });
 
-  // Test case 3
   it("Owner increment and decrement the count twice", async function () {
     await this.contract.increment();
     await this.contract.increment();
@@ -40,10 +37,16 @@ contract("MyContract", function (accounts) {
     expect(curr_count.toString()).to.equal("0");
   });
 
-  // Test case 5
   it("Call increment with non-Owner account", async function () {
     await expectRevert(
       this.contract.increment({ from: other }),
+      "Ownable: caller is not the owner"
+    );
+  });
+
+  it("Call decrement with non-Owner account", async function () {
+    await expectRevert(
+      this.contract.decrement({ from: other }),
       "Ownable: caller is not the owner"
     );
   });
