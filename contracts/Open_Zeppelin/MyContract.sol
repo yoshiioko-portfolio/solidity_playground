e3c1c07b0e25060f47ca3719051c37675b244ec7
yoshiioko <acardoza@pdx.edu>
2022-04-29 22:30:00 -0700

    Update: documenting code

--- contracts/Open_Zeppelin/MyContract.sol
+++ contracts/Open_Zeppelin/MyContract.sol
@@ -4,18 +4,33 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @dev Contract implements a simple counter that has access
+ * control via the use of OpenZeppelin Ownable contract.
+ */
 contract MyContract is Ownable {
     uint count = 0;
 
+    /**
+     * @dev Returns the current value of the counter.
+     */
     function getCount() public view returns (uint) {
         // Anyone can call this function
         return count;
     }
 
+    /**
+     * @dev Increments the counter.
+     * Can only be called by the contract owner
+     */
     function increment() public onlyOwner {
         count = count + 1;
     }
 
+    /**
+     * @dev Decrements the counter.
+     * Can only be called by the contract owner
+     */
     function decrement() public onlyOwner {
         require(count > 0, "Unable to decrement Zero value!");
         count = count - 1;
